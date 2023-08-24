d97d5d1d07b97e06183bd813fa63dc140384977d
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-08-24 18:25:46 +0300

    Update Slider.jsx

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,14 +1,13 @@
-import React, { useContext } from "react";
-import SimpleSlider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-import { SliderContainer } from "./Slider.styled";
+import React, { useContext } from 'react';
+import SimpleSlider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { SliderContainer } from './Slider.styled';
 import { Context } from 'components/App';
 import { MdZoomOutMap } from 'react-icons/md';
 
 export const Slider = ({ images, title }) => {
   const context = useContext(Context);
-
 
   const settings = {
     dots: true,
@@ -21,30 +20,19 @@ export const Slider = ({ images, title }) => {
   return (
     <div>
       <SimpleSlider {...settings}>
-        <SliderContainer >
+        <SliderContainer>
           <MdZoomOutMap onClick={() => context.open(images.img1)} />
-          <img 
-            src={process.env.PUBLIC_URL + images.img1} 
-            alt={`${title}-1`} 
-          />
+          <img src={process.env.PUBLIC_URL + images.img1} alt={`${title}-1`} />
         </SliderContainer>
         <SliderContainer>
-              <MdZoomOutMap onClick={() => context.open(images.img2)} />
-          <img 
-            src={process.env.PUBLIC_URL + images.img2} 
-            alt={`${title}-2`} 
-          />
+          <MdZoomOutMap onClick={() => context.open(images.img2)} />
+          <img src={process.env.PUBLIC_URL + images.img2} alt={`${title}-2`} />
         </SliderContainer>
         <SliderContainer>
-             <MdZoomOutMap onClick={() => context.open(images.img3)} />
-          <img 
-            src={process.env.PUBLIC_URL + images.img3} 
-            alt={`${title}-3`} 
-          />
+          <MdZoomOutMap onClick={() => context.open(images.img3)} />
+          <img src={process.env.PUBLIC_URL + images.img3} alt={`${title}-3`} />
         </SliderContainer>
       </SimpleSlider>
     </div>
   );
-}
-
-const styleZoomButton = { position: "absolute", bottom: "10", right: "10", padding: '10px', backgroundColor: 'black', borderRadius: "50%", fill: "white", cursor: "pointer"}
+};
