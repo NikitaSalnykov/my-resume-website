2e290f9ec0c01d265eede5b54b2721c480e08371
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-09-05 20:30:45 +0300

    add new projects and skills

--- src/components/ProjectList/ProjectsList.jsx
+++ src/components/ProjectList/ProjectsList.jsx
@@ -5,32 +5,45 @@ import { ProjectsButtonContainer } from './ProjectList.styled';
 
 export const ProjectList = ({ projects }) => {
   const [isAllOpen, setIsAllOpen] = useState(false);
-  const selectedProjects = [projects[2], projects[3], projects[0]]
-
+  const selectedProjects = [projects[2], projects[5], projects[0], projects[3]];
 
   const onAllProjects = () => {
     setIsAllOpen(true);
-  }
-    const onSelectedProjects = () => {
+  };
+  const onSelectedProjects = () => {
     setIsAllOpen(false);
-  }
+  };
 
   return (
     <Section>
       <Container>
-          <Title>Projects</Title>
+        <Title>Projects</Title>
         <ProjectsButtonContainer>
-        <button onClick={onSelectedProjects} disabled={!isAllOpen}>Selected</button>
-        <button onClick={onAllProjects} disabled={isAllOpen}>All projects ({projects.length})</button>
+          <button onClick={onSelectedProjects} disabled={!isAllOpen}>
+            Selected
+          </button>
+          <button onClick={onAllProjects} disabled={isAllOpen}>
+            All projects ({projects.length})
+          </button>
         </ProjectsButtonContainer>
-         <ul>
-      {isAllOpen ? projects.map(project => (
-        <ProjectCard key={project.id} project={project} isAllOpen={isAllOpen}/>
-      )) : selectedProjects.map(project => (
-        <ProjectCard key={project.id} project={project} isAllOpen={isAllOpen}/>
-      ))}
+        <ul>
+          {isAllOpen
+            ? projects.map(project => (
+                <ProjectCard
+                  key={project.id}
+                  project={project}
+                  isAllOpen={isAllOpen}
+                />
+              ))
+            : selectedProjects.map(project => (
+                <ProjectCard
+                  key={project.id}
+                  project={project}
+                  isAllOpen={isAllOpen}
+                />
+              ))}
         </ul>
       </Container>
-   </Section>
+    </Section>
   );
 };
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,82 +1,91 @@
-import { Container, Section } from "components/Resume.styled"
-import { useState } from "react";
-import { SkillsContainer, SkillsList } from "./Skills.styled";
+import { Container, Section } from 'components/Resume.styled';
+import { useState } from 'react';
+import { SkillsContainer, SkillsList } from './Skills.styled';
 
 export const Skills = () => {
   const techStack = [
-    "HTML5/CSS3",
-    "SCSS",
-    "Responsive/Adaptive Design",
-    "GIT",
-    "JavaScript",
-    "TypeScript",
-    "React",
-    "Styled-components",
-    "REST API",
-    "Webpack",
-    "Parcel",
-    "Vite"
+    'HTML5/CSS3',
+    'SCSS',
+    'Responsive/Adaptive Design',
+    'GIT',
+    'JavaScript',
+    'TypeScript',
+    'React',
+    'Styled-components',
+    'REST API',
+    'Material-UI/Tailwind',
+    'Axios',
+    'Redux Toolkit',
+    'Webpack',
+    'Parcel',
+    'Vite',
   ];
-  
+
   const softSkills = [
-    "Teamwork",
-    "Adaptability",
-    "Open-mindedness",
-    "Scrum Basics"
+    'Teamwork',
+    'Adaptability',
+    'Open-mindedness',
+    'Scrum Basics',
   ];
-  
+
   const languages = [
-    { language: "English", level: "Intermediate" },
-    { language: "Ukrainian", level: "Native" },
-    { language: "Russian", level: "Native" }
+    { language: 'English', level: 'Intermediate' },
+    { language: 'Ukrainian', level: 'Native' },
+    { language: 'Russian', level: 'Native' },
   ];
 
   const [selectedSkill, setSelectedSkill] = useState(null);
 
-  const handleSkillDescription = (skill) => {
+  const handleSkillDescription = skill => {
     console.log(skill, selectedSkill);
-    if(skill === selectedSkill) return setSelectedSkill('');
+    if (skill === selectedSkill) return setSelectedSkill('');
     setSelectedSkill(skill);
-  }
+  };
 
-  const getSkillDescription  = (skill) => {
-switch (skill) {
-  case 'HTML5/CSS3':
-    return 'Semantic HTML, CSS Grid/Flexbox layouts, responsive & accessible forms';
-  case 'SCSS':
-    return 'Modular styles using SCSS, nesting, variables, mixins';
-  case 'Responsive/Adaptive Design':
-    return 'Designing adaptable layouts with media queries and fluid grids';
-  case 'GIT':
-    return 'Version control, branching, merging, collaboration with Git';
-  case 'JavaScript':
-    return 'Interactive elements, DOM manipulation, async programming';
-  case 'TypeScript':
-    return 'Static typing, interfaces, types, enhanced code quality';
-  case 'React':
-    return 'Building dynamic UIs, state management, React Router';
-  case 'Styled-components':
-    return 'Styling React components with dynamic styles';
-  case 'REST API':
-    return 'Consuming and designing RESTful APIs, HTTP requests';
-  case 'Webpack':
-    return 'Bundling and optimizing assets with Webpack';
-  case 'Parcel':
-    return 'Zero-config web application bundler for quick setups';
-  case 'Vite':
-    return 'Fast build tool focused on speed for modern web projects';
-  case 'Teamwork':
-    return 'Collaborative and effective team player, communication skills';
-  case 'Adaptability':
-    return 'Quickly adapt to new challenges and dynamic environments';
-  case 'Open-mindedness':
-    return 'Willingness to consider diverse perspectives and ideas';
-  case 'Scrum Basics':
-    return 'Fundamental understanding of Scrum framework for agile project management';
-  default:
-    return '';
-}
-  }
+  const getSkillDescription = skill => {
+    switch (skill) {
+      case 'HTML5/CSS3':
+        return 'Semantic HTML, CSS Grid/Flexbox layouts, responsive & accessible forms';
+      case 'SCSS':
+        return 'Modular styles using SCSS, nesting, variables, mixins';
+      case 'Responsive/Adaptive Design':
+        return 'Designing adaptable layouts with media queries and fluid grids';
+      case 'GIT':
+        return 'Version control, branching, merging, collaboration with Git';
+      case 'JavaScript':
+        return 'Interactive elements, DOM manipulation, async programming';
+      case 'TypeScript':
+        return 'Static typing, interfaces, types, enhanced code quality';
+      case 'React':
+        return 'Building dynamic UIs, state management, React Router';
+      case 'Styled-components':
+        return 'Styling React components with dynamic styles';
+      case 'REST API':
+        return 'Consuming and designing RESTful APIs, HTTP requests';
+      case 'Axios':
+        return 'Making HTTP requests, handling responses with Axios';
+      case 'Redux Toolkit':
+        return 'State management with Redux Toolkit, reducers, actions';
+      case 'Material-UI/Tailwind':
+        return 'Creating UI components with Material-UI and Tailwind ';
+      case 'Webpack':
+        return 'Bundling and optimizing assets with Webpack';
+      case 'Parcel':
+        return 'Zero-config web application bundler for quick setups';
+      case 'Vite':
+        return 'Fast build tool focused on speed for modern web projects';
+      case 'Teamwork':
+        return 'Collaborative and effective team player, communication skills';
+      case 'Adaptability':
+        return 'Quickly adapt to new challenges and dynamic environments';
+      case 'Open-mindedness':
+        return 'Willingness to consider diverse perspectives and ideas';
+      case 'Scrum Basics':
+        return 'Fundamental understanding of Scrum framework for agile project management';
+      default:
+        return '';
+    }
+  };
 
   return (
     <Section>
@@ -84,53 +93,59 @@ switch (skill) {
         <SkillsContainer>
           <h3>Tech Stack</h3>
           <SkillsList>
-  {techStack.map((skill, index) => (
-    <>
-    { selectedSkill === skill ? (
-       <li  style={{background: '#1e1e1e', color: 'white',
-    textAlign: 'center', padding: '12px'}} key={`soft-${index}`}>
-        <p onClick={() => handleSkillDescription(skill)}>
-          <b>{skill}</b>
-          <span>
-            {` - ` + getSkillDescription(selectedSkill)}
-          </span>
-        </p>
-            </li>
-    ) : (
-           <li key={`soft-${index}`}>
-        <p onClick={() => handleSkillDescription(skill)}>
-          {skill}
-        </p>
-            </li>
-      )}
-</>
-  ))}
+            {techStack.map((skill, index) => (
+              <>
+                {selectedSkill === skill ? (
+                  <li
+                    style={{
+                      background: '#1e1e1e',
+                      color: 'white',
+                      textAlign: 'center',
+                      padding: '12px',
+                    }}
+                    key={`soft-${index}`}
+                  >
+                    <p onClick={() => handleSkillDescription(skill)}>
+                      <b>{skill}</b>
+                      <span>{` - ` + getSkillDescription(selectedSkill)}</span>
+                    </p>
+                  </li>
+                ) : (
+                  <li key={`soft-${index}`}>
+                    <p onClick={() => handleSkillDescription(skill)}>{skill}</p>
+                  </li>
+                )}
+              </>
+            ))}
           </SkillsList>
         </SkillsContainer>
         <SkillsContainer>
           <h3>Soft Skills</h3>
           <SkillsList>
-  {softSkills.map((skill, index) => (
-    <>
-    { selectedSkill === skill ? (
-       <li  style={{background: '#1e1e1e', color: 'white',
-    textAlign: 'center', padding: '12px'}} key={`soft-${index}`}>
-        <p onClick={() => handleSkillDescription(skill)}>
-          <b>{skill}</b>
-          <span>
-            {` - ` + getSkillDescription(selectedSkill)}
-          </span>
-        </p>
-            </li>
-    ) : (
-           <li key={`soft-${index}`}>
-        <p onClick={() => handleSkillDescription(skill)}>
-          {skill}
-        </p>
-            </li>
-      )}
-</>
-  ))}
+            {softSkills.map((skill, index) => (
+              <>
+                {selectedSkill === skill ? (
+                  <li
+                    style={{
+                      background: '#1e1e1e',
+                      color: 'white',
+                      textAlign: 'center',
+                      padding: '12px',
+                    }}
+                    key={`soft-${index}`}
+                  >
+                    <p onClick={() => handleSkillDescription(skill)}>
+                      <b>{skill}</b>
+                      <span>{` - ` + getSkillDescription(selectedSkill)}</span>
+                    </p>
+                  </li>
+                ) : (
+                  <li key={`soft-${index}`}>
+                    <p onClick={() => handleSkillDescription(skill)}>{skill}</p>
+                  </li>
+                )}
+              </>
+            ))}
           </SkillsList>
         </SkillsContainer>
         <SkillsContainer>
@@ -138,7 +153,9 @@ switch (skill) {
           <SkillsList>
             {languages.map((skill, index) => (
               <li key={`lang-${index}`}>
-                <p>{skill.language} - {skill.level}</p>
+                <p>
+                  {skill.language} - {skill.level}
+                </p>
               </li>
             ))}
           </SkillsList>
